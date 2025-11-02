a047e47d660b2ac3d621229d301d2cb231383398
MD. IMRAN HOSSAIN <ih110602005@gmail.com>
2025-11-03 00:49:46 +0600

    product list done

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -54,10 +54,22 @@ const categories = [
   },
 ];
 const Categories = () => {
+    const searchParams = useSearchParams()
+    const pathname = usePathname()
+    const selectedCategory = searchParams.get("category")
+    
+    const router = useRouter()
+    const handleChange = (slug)=>{
+        const params = new URLSearchParams(searchParams)
+        params.set("category", slug || "all")
+        router.push(`${pathname}/?${params.toString()}`, {scroll:false})
+    }
   return (
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm'>
         {categories.map(category=>(
-            <div className='flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md' key={category.name}>
+            <div className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${category.slug === selectedCategory? "bg-white":"text-gray-500"}`} key={category.name}
+            onClick={()=>handleChange(category.slug)}
+            >
                 {category.icon}
                 {category.name}
             </div>
